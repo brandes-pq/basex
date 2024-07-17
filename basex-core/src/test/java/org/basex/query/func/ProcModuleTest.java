8e5d3972d825d003e1f154b4cc8e5781ef6615b8
Hauke Brandes <hauke.brandes@parsqube.de>
2024-07-17 12:12:03 +0200

    Add tests for proc:system options

--- basex-core/src/test/java/org/basex/query/func/ProcModuleTest.java
+++ basex-core/src/test/java/org/basex/query/func/ProcModuleTest.java
@@ -1,9 +1,15 @@
 package org.basex.query.func;
 
+import static java.lang.String.format;
 import static org.basex.query.QueryError.*;
 import static org.basex.query.func.Function.*;
 
 import java.io.*;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 import org.basex.*;
 import org.basex.util.*;
@@ -73,7 +79,88 @@ public final class ProcModuleTest extends SandboxTest {
     query(func.args("java", "-version"), "");
     query("try { " + func.args("java", "x") + "} catch proc:* { 'error' }", "error");
 
-    error(func.args("java", "-version", " map { 'encoding': 'xx' }"), PROC_ENCODING_X);
     error(func.args("a b c"), PROC_ERROR_X);
+
+    // test encoding option
+    error(func.args("java", "-version", " map { 'encoding': 'xx' }"), PROC_ENCODING_X);
+    query(func.args("java",
+                    format(" ('-cp', 'target/test-classes', '%s', 'UTF-16')", writeEnc.class.getName()),
+                    " map { 'encoding': 'UTF-16'}"),
+            "\u03b1\n");
+    query(func.args("java",
+                    format(" ('-cp', 'target/test-classes', '%s', 'UTF-8')", writeEnc.class.getName()),
+                    " map { 'encoding': 'UTF-8'}"),
+            "\u03b1\n");
+
+    // test environment option
+    query(func.args("java",
+                    format(" ('-cp', 'target/test-classes', '%s')", env.class.getName()),
+                    " map { 'environment': map { 'FOO': 'bar' }}"),
+            "FOO=bar\n");
+
+    // test timeout option
+    error(func.args("java",
+                    format(" ('-cp', 'target/test-classes', '%s', '2')", sleep.class.getName()),
+                    " map { 'timeout': '1'}"),
+            PROC_TIMEOUT);
+    query(func.args("java",
+                  format(" ('-cp', 'target/test-classes', '%s', '0')", sleep.class.getName()),
+                  " map { 'timeout': '10'}"),
+            "");
+ 
+    // test dir option
+    Path dir = FileSystems.getDefault().getRootDirectories().iterator().next();
+    Path testClassPath = Path.of("target/test-classes").toAbsolutePath();
+    assert !dir.toString().contains("'");
+    assert !testClassPath.toString().contains("'");
+    query(func.args("java",
+                    format(" ('-cp', '%s', '%s')", testClassPath, pwd.class.getName()),
+                    format(" map { 'dir': '%s' }", dir)),
+            dir + "\n");
+  }
+
+  // used as external process in proc:system tests.
+  public static class writeEnc {
+    public static void main(String[] args) throws InterruptedException, UnsupportedEncodingException {
+      String enc = args[0];
+      System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out), true, enc));
+      // Greek Small Letter Alpha
+      System.out.println("\u03b1");
+    }
+  }
+
+  // used as external process in proc:system tests.
+  public static class env {
+    public static void main(String[] args) {
+
+      Set<String> ignoreVars = new HashSet<>();
+
+      // this may be set on every process in macos
+      ignoreVars.add("__CF_USER_TEXT_ENCODING");
+
+      // this may be set on every process in Windows
+      ignoreVars.add("SystemRoot");
+
+      for (Map.Entry<String, String> e : System.getenv().entrySet()) {
+        if (ignoreVars.contains(e.getKey())) {
+          continue;
+        }
+        System.out.println(e.getKey() + "=" + e.getValue());
+      }
+    }
+  }
+
+  // used as external process in proc:system tests.
+  public static class pwd {
+    public static void main(String[] args) {
+      System.out.println(System.getProperty("user.dir"));
+    }
+  }
+
+  // used as external process in proc:system tests.
+  public static class sleep {
+    public static void main(String[] args) throws InterruptedException {
+      Thread.sleep(1000 * Long.parseLong(args[0]));
+    }
   }
 }
